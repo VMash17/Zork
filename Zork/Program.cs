07ef797b2ab75b22fc7168a4945d1d240ac18156
VMash17 <vmash17@knights.ucf.edu>
2022-08-29 15:24:59 -0400

    Zork 1.2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -9,19 +9,53 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-            if (inputString == "QUIT")
+
+            Commands command = ToCommand(inputString);
+
+            Console.WriteLine(command);
+
+            if (command == Commands.QUIT)
             {
                 Console.WriteLine("Thank you for playing.");
             }
-            else if (inputString == "LOOK")
+            else if (command == Commands.LOOK)
             {
                 Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            }
+            else if (command == Commands.NORTH)
+            {
+                Console.WriteLine("You moved North.");
+            }
+            else if (command == Commands.SOUTH)
+            {
+                Console.WriteLine("You moved South.");
+            }
+            else if (command == Commands.WEST)
+            {
+                Console.WriteLine("You moved West.");
+            }
+            else if (command == Commands.EAST)
+            {
+                Console.WriteLine("You moved East.");
             }
             else
             {
                 Console.WriteLine($"Unrecognized command. {inputString}");
             }
+
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+            if (Enum.TryParse<Commands>(commandString, true, out Commands command))
+            {
+                return command;
+            }
+            else
+            {
+                return Commands.UNKNOWN;
+            }
+
         }
     }
 }
