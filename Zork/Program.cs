6f34f1b4f6797eb34ff24e21fbbc19b05104b013
VMash17 <vmash17@knights.ucf.edu>
2022-08-31 15:36:28 -0400

    Zork 1.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,41 +8,37 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine().Trim();
+            bool isRunning = true;
+            
+            while (isRunning == true)
+            {
+                Console.Write("> ");
+                string inputString = Console.ReadLine().Trim();
 
-            Commands command = ToCommand(inputString);
+                Commands command = ToCommand(inputString);
 
-            Console.WriteLine(command);
-
-            if (command == Commands.QUIT)
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (command == Commands.LOOK)
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-            else if (command == Commands.NORTH)
-            {
-                Console.WriteLine("You moved North.");
-            }
-            else if (command == Commands.SOUTH)
-            {
-                Console.WriteLine("You moved South.");
-            }
-            else if (command == Commands.WEST)
-            {
-                Console.WriteLine("You moved West.");
-            }
-            else if (command == Commands.EAST)
-            {
-                Console.WriteLine("You moved East.");
-            }
-            else
-            {
-                Console.WriteLine($"Unrecognized command. {inputString}");
-            }
-
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing.";
+                        isRunning = false;
+                        break;
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.WEST:
+                    case Commands.EAST:
+                        outputString = $"You moved {command}.";
+                        break;
+                    default:
+                        outputString = "Unrecognized command.";
+                        break;
+                }
+                Console.WriteLine(outputString);
+            }  
         }
 
         private static Commands ToCommand(string commandString)
