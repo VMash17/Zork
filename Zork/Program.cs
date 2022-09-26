0bcecc33485468ae201a01a20a31143c168e0c9d
VMash17 <vmash17@knights.ucf.edu>
2022-09-26 19:45:51 -0400

    Zork 3.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Zork
 {
@@ -7,35 +8,32 @@ namespace Zork
     {
         private static Room CurrentRoom
         {
-            get 
-            { 
+            get
+            {
                 return rooms[Location.Row, Location.Column];
-                   
+
             }
         }
 
         static void Main(string[] args)
         {
-            InitializeRoomDescriptions();
+            InitializeRoomDescriptions(@"Content\Rooms.txt");
 
             Console.WriteLine("Welcome to Zork!");
 
             Room previousRoom = null;
             bool isRunning = true;
-            
             while (isRunning == true)
             {
                 Console.WriteLine(CurrentRoom);
-
                 if (previousRoom != CurrentRoom)
                 {
                     Console.WriteLine(CurrentRoom.Description);
                     previousRoom = CurrentRoom;
                 }
+                Console.Write("> ");
 
-                Console.Write(">");
                 string inputString = Console.ReadLine().Trim();
-
                 Commands command = ToCommand(inputString);
 
                 string outputString;
@@ -45,9 +43,11 @@ namespace Zork
                         outputString = "Thank you for playing.";
                         isRunning = false;
                         break;
+
                     case Commands.LOOK:
                         outputString = CurrentRoom.Description;
                         break;
+
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.WEST:
@@ -61,12 +61,13 @@ namespace Zork
                             outputString = "The way is shut!";
                         }
                         break;
+
                     default:
-                        outputString = "Unknown command.";
+                        outputString = "Unrecognized command.";
                         break;
                 }
                 Console.WriteLine(outputString);
-            }  
+            }
         }
 
         private static Commands ToCommand(string commandString)
@@ -79,27 +80,33 @@ namespace Zork
             {
                 return Commands.UNKNOWN;
             }
-
         }
-        private static void InitializeRoomDescriptions()
+
+        private static void InitializeRoomDescriptions(string roomsFileName)
         {
             var roomMap = new Dictionary<string, Room>();
+
             foreach (Room room in rooms)
             {
-                roomMap.Add(room.Name, room);
+                roomMap[room.Name] = room;
             }
+            
+            string[] lines = File.ReadAllLines(roomsFileName);
+            foreach (string line in lines)
+            {
+                const string fieldDelimiter = "##";
+                const int expectedFieldCount = 2;
 
-            roomMap["Rocky Trail"].Description = "You are on a rocky-strewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are the top of the Great Canyon on its south wall.";
+                string[] fields = line.Split(fieldDelimiter);
+                if (fields.Length != expectedFieldCount)
+                {
+                    throw new InvalidDataException("Invalid record.");
+                }
+                string name = fields[(int)Fields.Name];
+                string description = fields[(int)Fields.Description];
 
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-            roomMap["West of House"].Description = "This is a open field west of a white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner at the house there is a small window which is slightly ajar.";
-
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+                roomMap[name].Description = description;
+            }          
         }
 
         private static readonly Room[,] rooms = {
@@ -108,37 +115,40 @@ namespace Zork
             { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
 
-        private static (int Row, int Column) Location = (1,1);
+        private static (int Row, int Column) Location = (1, 1);
 
-        private static bool Move(Commands command)
+        static bool Move(Commands command)
         {
-            bool canMove = false;
-
+            bool canMove = true;
             switch (command)
             {
                 case Commands.NORTH when Location.Row < rooms.GetLength(0) - 1:
                     Location.Row++;
-                    canMove = true;
                     break;
 
                 case Commands.SOUTH when Location.Row > 0:
                     Location.Row--;
-                    canMove = true;
                     break;
 
                 case Commands.EAST when Location.Column < rooms.GetLength(1) - 1:
                     Location.Column++;
-                    canMove = true;
                     break;
 
                 case Commands.WEST when Location.Column > 0:
                     Location.Column--;
-                    canMove = true;
+                    break;
+
+                default:
+                    canMove = false;
                     break;
             }
-
             return canMove;
         }
 
+        private enum Fields
+        {
+            Name = 0,
+            Description = 1
+        }
     }
 }
